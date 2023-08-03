afe679a546574e8140956274174032759ec7f866
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-08-04 02:40:49 +0300

    javascript for page loader

---
+++ src/assets/js/style.js
@@ -0,0 +1,9 @@
+const spinnerWrapperEl = document.querySelector('.spinner_wrapper');
+
+window.addEventListener('load', () => {
+    spinnerWrapperEl.style.opacity = '0';
+
+    setTimeout(() => {
+        spinnerWrapperEl.style.display = 'none';
+    }, 200);
+})
